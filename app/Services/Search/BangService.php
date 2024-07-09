367a8e24d708861f748d174ad07913f65f0e0987
moon250 <mooneupho@gmail.com>
2024-07-09 19:33:12 +0200

    feat: added some bangs

--- app/Services/Search/BangService.php
+++ app/Services/Search/BangService.php
@@ -10,12 +10,18 @@ class BangService
      * @var string[]
      */
     private array $redirectBangs = [
-        '!g' => 'https://google.com/search?q=%s',
-        '!gh' => 'https://github.com/search?q=%s',
+        '!g' => 'https://google.com/search?q=%s', // Google search
+        '!gh' => 'https://github.com/search?q=%s', // Github search
+        // Deepl translation
         '!tr' => 'https://deepl.com/fr/translator#en/fr/%s',
         '!deepl' => 'https://deepl.com/fr/translator#en/fr/%s',
-        '!img' => 'https://duckduckgo.com/?q=%s&iax=images&ia=images',
-        '!gi' => 'https://google.com/search?q=%s&udm=2',
+        '!img' => 'https://duckduckgo.com/?q=%s&iax=images&ia=images', // DDG images
+        '!gi' => 'https://google.com/search?q=%s&udm=2', // Google Images
+        '!yt' => 'https://youtube.com/results?search_query=%s', // Youtube
+        // Google Maps
+        '!gmaps' => 'https://www.google.com/maps/preview?q=%s',
+        '!gm' => 'https://www.google.com/maps/preview?q=%s',
+        '!meteo' => 'https://www.meteociel.fr/prevville.php?action=getville&ville=%s',
     ];
 
     public function hasBang(string $query): string|false
