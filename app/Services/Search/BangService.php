3137f01313cfef3ec271d5043998b00be2e4883e
moon250 <mooneupho@gmail.com>
2024-07-23 07:52:00 +0200

    ref: add bangs and modified docker configuration

--- app/Services/Search/BangService.php
+++ app/Services/Search/BangService.php
@@ -12,22 +12,29 @@ class BangService
     private array $redirectBangs = [
         '!g' => 'https://google.com/search?q=%s', // Google search
         '!gh' => 'https://github.com/search?q=%s', // Github search
+
         // Deepl translation
         '!tr' => 'https://deepl.com/fr/translator#en/fr/%s',
         '!deepl' => 'https://deepl.com/fr/translator#en/fr/%s',
+
         '!img' => 'https://duckduckgo.com/?q=%s&iax=images&ia=images', // DDG images
         '!gi' => 'https://google.com/search?q=%s&udm=2', // Google Images
         '!yt' => 'https://youtube.com/results?search_query=%s', // Youtube
+
         // Google Maps
         '!gmaps' => 'https://www.google.com/maps/preview?q=%s',
         '!gm' => 'https://www.google.com/maps/preview?q=%s',
         '!meteo' => 'https://www.meteociel.fr/prevville.php?action=getville&ville=%s',
+
+        // Reddit
+        '!reddit' => 'https://www.reddit.com/search/?q=%s',
+        '!r' => 'https://www.reddit.com/search?q=%s',
     ];
 
     public function __construct()
     {
         // Mandatory because we loop over the bangs in order. It fixes this example key order : "!g", "!gi"
-        // "!g" would be matched first even though it should match "!gi"
+        // "!g" would match first even though it should match "!gi"
         uksort($this->redirectBangs, fn ($a, $b) => mb_strlen($b) <=> mb_strlen($a));
     }
 
